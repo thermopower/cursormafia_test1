c0c2a36ba5efe282cd85918766cc553bb28ccd1c
thermopower <higj00@gmail.com>
2025-09-09 05:16:49 +0900

    good

--- src/App.test.js
+++ src/App.test.js
@@ -1,15 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-<<<<<<< HEAD
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-=======
+
 test('renders top-center HUD with Score and Time', () => {
   render(<App />);
   const hud = screen.getByText(/Score:/i);
   expect(hud).toBeInTheDocument();
->>>>>>> 49d474ed161c959dc1e005abc617aabffcb9b32a
+
 });
